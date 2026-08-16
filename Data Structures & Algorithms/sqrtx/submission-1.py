9c87f449b075e459c15bfa251d289bc9fc1799be
Yug <yugsavaliya644@gmail.com>
2026-08-16 19:11:21 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(0,x+1):
+            if i*i==x:
+                return i
+            
+            if i*i<x:
+                continue
+            else:
+                return i-1
